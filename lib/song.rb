954e20cc54d53b72c1be587103a018d9dca6ef00
Ian Mayton <ian.mayton@gmail.com>
2020-07-22 16:00:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,4 +18,25 @@ class Song
   def Song.count
     @@count
   end
+  
+  def Song.artists
+    unique_artists = []
+    @@artists.each do |artist|
+      if !unique_artists.include?(artist)
+        unique_artists << artist 
+      end
+    end
+    unique_artists
+  end
+  
+  def Song.genres
+    unique_genres = []
+    @@genres.each do |genre| 
+      if !unique_genres.include?(genre)
+        unique_genres << genre 
+      end 
+    end 
+    unique_genres
+  end 
+  
 end
